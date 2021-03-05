5511ebe0b6cd57cc79f5d81fee201e2f0f99658a
郭文猛 <guowm@5fun.com>
2021-03-05 09:58:22 +0800

    feat: add: log

--- cloud-customer-service/src/main/java/com/bkhech/cloud/customer/feign/FeignService.java
+++ cloud-customer-service/src/main/java/com/bkhech/cloud/customer/feign/FeignService.java
@@ -2,16 +2,17 @@ package com.bkhech.cloud.customer.feign;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Feign service
  * @author guowm
  * @date 2021/3/4
+ * @FeignClient中参数：
+ * path 等价于 @RequestMapping("producer")
  */
-@FeignClient(name = "producer-service")
-@RequestMapping("producer")
+
+@FeignClient(name = "producer-service", path = "producer")
 public interface FeignService {
 
     /**
