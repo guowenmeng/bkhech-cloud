4c5dedb324119a4a751ea6f8403140741d2bf33e
郭文猛 <guowm@5fun.com>
2021-03-30 16:21:01 +0800

    feat: Spring cloud Stream Rabbitmq binder:  手动ack消息

--- cloud-provider-service/src/main/java/com/bkhech/cloud/provider/stream/StreamReceiver.java
+++ cloud-provider-service/src/main/java/com/bkhech/cloud/provider/stream/StreamReceiver.java
@@ -1,10 +1,15 @@
 package com.bkhech.cloud.provider.stream;
 
+import com.rabbitmq.client.Channel;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.support.AmqpHeaders;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Sink;
-import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.handler.annotation.Header;
+
+import java.io.IOException;
 
 /**
  * sc stream 接收者
@@ -20,9 +25,24 @@ public class StreamReceiver {
      * @param value
      */
     @StreamListener(target = Sink.INPUT) //监听input这个消息队列, 必须定义相应的Input。
-    public void handle(String value) {
-        log.info("[StreamReceiver 消息] 接收到MQ消息: {}", value);
+    public void handle(Message value, @Header(AmqpHeaders.CHANNEL) Channel channel, @Header(AmqpHeaders.DELIVERY_TAG) long deliveryTag) {
+        log.info("[StreamReceiver 消息] 接收到MQ消息: {}", value.getPayload());
+        try {
+            channel.basicAck(deliveryTag, true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
+    /**
+     * 监听 binding 为 Sink.INPUT 的消息
+     * @param value
+     */
+//    @StreamListener(target = Sink.INPUT) //监听input这个消息队列, 必须定义相应的Input。
+//    public void handle(String value) {
+//        log.info("[StreamReceiver 消息] 接收到MQ消息: {}", value);
+//        throw new RuntimeException("Intentional exception");
+//    }
 
     // 若方法签名有返回值，必须指定@SendTo注解
 //    @StreamListener(target = Sink.INPUT)
