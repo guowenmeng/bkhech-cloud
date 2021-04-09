cbc96f492b3679e729df563122a140df8c13700e
郭文猛 <guowm@5fun.com>
2021-04-09 20:06:47 +0800

    feat: add spring cloud gateway server 利用GatewayFilter实现请求时间记录功能

---
+++ cloud-gateway-server/src/main/java/com/bkhech/cloud/gateway/server/filter/gatewaylfilter/RequestTimeGatewayFilterFactory.java
@@ -0,0 +1,70 @@
+package com.bkhech.cloud.gateway.server.filter.gatewaylfilter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 请求时间过滤器工厂
+ * @author guowm
+ * @date 2021/4/9
+ */
+@Slf4j
+@Component
+public class RequestTimeGatewayFilterFactory extends AbstractGatewayFilterFactory<RequestTimeGatewayFilterFactory.Config> {
+    private static final String REQUEST_TIME_BEGIN = "requestTimeBegin";
+    private static final String KEY = "withParams";
+
+    public RequestTimeGatewayFilterFactory() {
+        super(Config.class);
+    }
+
+    @Override
+    public List<String> shortcutFieldOrder() {
+        return Arrays.asList(KEY);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+            exchange.getAttributes().put(REQUEST_TIME_BEGIN, System.currentTimeMillis());
+            return chain.filter(exchange).then(
+                    Mono.fromRunnable(() -> {
+                        Long startTime = exchange.getAttribute(REQUEST_TIME_BEGIN);
+                        if (startTime != null) {
+                            StringBuilder sb = new StringBuilder(exchange.getRequest().getURI().getRawPath())
+                                    .append(" elapseTime : ")
+                                    .append(System.currentTimeMillis() - startTime)
+                                    .append("ms");
+                            if (config.isWithParams()) {
+                                sb.append(" params:").append(exchange.getRequest().getQueryParams());
+                            }
+                            log.info(sb.toString());
+                        }
+                    })
+            );
+        };
+    }
+
+    public static class Config {
+
+        private boolean withParams;
+
+        public boolean isWithParams() {
+            return withParams;
+        }
+
+        public void setWithParams(boolean withParams) {
+            this.withParams = withParams;
+        }
+
+    }
+}
+
